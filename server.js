2fb029b028c3d0894868b10f3fea0671bdea03b7
Camerone Stoney <Cameronestoney@yahoo.com>
2019-07-15 21:05:33 -0700

    server completed

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller")
+app.use(routes);
+
 app.listen(PORT,function(){
     console.log("server is listening on:http://localhost: "+ PORT)
-})
+});
