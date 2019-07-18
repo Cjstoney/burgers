0b8cadbde96bb3de2380a01308c1bdce4f5dbdfd
Camerone Stoney <Cameronestoney@yahoo.com>
2019-07-18 11:30:31 -0700

    complete

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,29 @@
-var orm = require('orm');
+var orm = require('.../config/orm');
 
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+
+    insertOne: function(cos, vals, cb){
+        orm.selectAll("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+
+    updateOne: function(objColsVals, condition, cb){
+        orm.selectAll("burgers", objColsVals, condition, function(res){
+            cb(res);
+        });
+    },
+
+    DeleteOne: function(condition, cb){
+        orm.selectAll("burgers", condition, function(res){
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
